4e16d90560b997c094971af1ef34751c40a41925
devmonzer08 <142656890+devmonzer08@users.noreply.github.com>
2023-09-28 10:25:33 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int("15170853")
+API_HASH = ("33f17f7ccfa43fd779489dca8ab67e34")
+
+BOT_TOKEN = ("6519075070:AAEsAA_-qZcvU0WMg0ko-_FerK-223ibbjE")
+OWNER_ID = int("5593884330")
+
+MONGO_DB_URI = ("mongodb+srv://islam2005:islam2005@cluster0.rdjot.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+MUST_JOIN =("V_P_N_8")
